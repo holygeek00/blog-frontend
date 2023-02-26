bf29eaffacf0ab5f6f07ab061a875204e9702d16
liuqingfeng <719993988@qq.com>
2023-02-26 18:23:56 +0800

    fixup

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -8,7 +8,7 @@
         <div class="dropdown dropdown-end">
           <label tabindex="0" class="btn btn-ghost btn-circle avatar">
             <div class="w-10 rounded-full">
-              <img src="../../public/naassh-icon.jpeg" />
+              <img src="/public/naassh-icon.jpeg" />
             </div>
           </label>
           <ul
