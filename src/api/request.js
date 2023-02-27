38f30544c829b4fd1c1968968495177150621765
liuqingfeng <719993988@qq.com>
2023-02-27 14:17:16 +0800

    fixup

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,5 @@
 import axios from "axios"
-import.meta.env.STRAPI_KEY
-import.meta.env.STRAPI_URL
+
 // 引入axios模块
 
 const auth = 'Bearer ' + '777632157a7b1af6035233d3c6c0f6e42a502f2998314a5383ea8b6355f36f6a8679f4dd36ec7711b6d291df2d701f8dbef26f56adc02a31a336a5d767143c4cdd2b82965be39ddaa5774ca1fcc981e91333ae98e89e401a9ddd5712afe6c9b469ced01befaa29f96d0f3ac1f2c46ca5fa0b58dbf9dc90799dda8cea4238ecfd'
