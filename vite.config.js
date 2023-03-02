e89150f6cddb0d8705a8c0fe3353528cb0d4ec01
liuqingfeng <719993988@qq.com>
2023-03-02 23:06:13 +0800

    fixup

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueRouter from 'vue-router'
 import AutoImport from 'unplugin-auto-import/vite'
 import './src/assets/index.css'
+
+import dotenv from 'dotenv'
+
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), AutoImport({
