0430841162afde6a9c2fe1f4efbd00346bb6c3b8
liuqingfeng <719993988@qq.com>
2023-03-02 23:11:26 +0800

    fixup

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: AboutMe
+      component: () => import('@/views/aboutMe.vue')
     }
   ]
 })
